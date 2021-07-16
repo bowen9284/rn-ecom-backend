6ad398e6233db029c0fefc260dbb6c71be07e627
Matt Bowen <bowen9284@gmail.com>
2021-07-16 09:57:48 -0400

    update server config for heroku

--- app.js
+++ app.js
@@ -50,11 +50,6 @@ connect(connectionUrl, {
     console.log(err);
   });
 
-// Dev
-app.listen(3000, () => {
-  console.log('server is running http:///localhost:3000');
-});
-
 // Prod
 var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
